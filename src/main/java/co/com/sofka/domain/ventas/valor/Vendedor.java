58683ec0a58e58d06edc7f681f7c4985244f0e6d
ruizjose147 <ruizjose_147@hotmail.com>
2022-03-08 20:16:20 -0500

    Se modifica la entidad cliente, se crea el objeto de valor clienteId, se crea el objeto de valor telefono, se crea la clase carrito

--- src/main/java/co/com/sofka/domain/ventas/valor/Vendedor.java
+++ src/main/java/co/com/sofka/domain/ventas/valor/Vendedor.java
@@ -5,7 +5,7 @@ import co.com.sofka.domain.generic.ValueObject;
 import java.util.UUID;
 
 public class Vendedor implements ValueObject<String> {
-    UUID vendedorId = UUID.randomUUID();
+    private final UUID vendedorId = UUID.randomUUID();
 
     @Override
     public String value() {
