6cd1ded657b47b3328a91a1adbd6973217480da0
EdMass <zairmass@gmail.com>
2022-03-08 23:29:22 -0500

    modificacion del OV Precio para obtener el valor, modificacion del OV Total generamos los metodos, modificaion de la entidad Carrito, geeramos metodos

--- src/main/java/co/com/sofka/domain/ventas/Carrito.java
+++ src/main/java/co/com/sofka/domain/ventas/Carrito.java
@@ -1,4 +1,37 @@
 package co.com.sofka.domain.ventas;
 
-public class Carrito {
+import co.com.sofka.domain.generic.Entity;
+import co.com.sofka.domain.generic.Identity;
+import co.com.sofka.domain.inventario.Producto;
+import co.com.sofka.domain.inventario.valor.ProductoID;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Carrito extends Entity {
+
+    private List<Producto> productosCarrito = new ArrayList<>();
+    
+    public Carrito(Identity carritoId) {
+        super(carritoId);
+    }
+
+    public void agregarProducto(Producto producto){
+        productosCarrito.add(producto);
+    }
+
+    public void eliminarProducto(ProductoID productoID){
+        for (Producto producto:productosCarrito) {
+            if(producto.identity().equals(productoID)){
+                productosCarrito.remove(producto);
+            }
+            else{
+                System.out.println("Producto no encontrado");
+            }
+        }
+    }
+
+    public List<Producto> listarProductos(){
+        return productosCarrito;
+    }
+
 }
--- src/main/java/co/com/sofka/domain/ventas/valor/Total.java
+++ src/main/java/co/com/sofka/domain/ventas/valor/Total.java
@@ -1,14 +1,24 @@
 package co.com.sofka.domain.ventas.valor;
 
+import co.com.sofka.domain.generic.Identity;
 import co.com.sofka.domain.generic.ValueObject;
+import co.com.sofka.domain.inventario.Producto;
+import co.com.sofka.domain.ventas.Carrito;
 
 
 public class Total implements ValueObject<Double>  {
 
+    Identity carritoId;
+    Carrito carrito = new Carrito(carritoId);
+    Double contador = 0.0;
+
 
     @Override
     public Double value() {
-        return 0.0;
+        for (Producto producto: carrito.listarProductos()) {
+            contador = contador + producto.Precio().getPrice();
+        }
+        return contador;
     }
 
 }
